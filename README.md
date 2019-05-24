be9cc3ed6b72a7270dae36187c9e8a21fa17431d
Sherry Freitas <sherry.freitas@gmail.com>
2019-05-23 22:17:35 -0700

    adds initial files

---
+++ README.md
@@ -0,0 +1,17 @@
+# Change Calculator
+
+## To install:
+```
+npm install
+```
+
+## To run:
+```
+npm start
+```
+Then go to http://127.0.0.1:8080/ in your browser.
+
+## To run tests:
+```
+npm test
+```
--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-// Write your JavaScript here
+document.getElementById('calculate-change').onclick = handleCalculation;
+
+const money = [
+    ['onehundreddollars', 100], 
+    ['fiftydollars', 50], 
+    ['twentydollars', 20], 
+    ['tendollars', 10], 
+    ['fivedollars', 5], 
+    ['dollars', 1], 
+    ['quarters', 0.25], 
+    ['dimes', 0.1], 
+    ['nickels', 0.05], 
+    ['pennies', 0.01]
+];
+
+function handleCalculation() {
+    document.getElementById('error').innerHTML = '';
+    document.getElementById('success').innerHTML = '';
+    let owed =  parseFloat(document.getElementById('amount-due').value);
+    let given = parseFloat(document.getElementById('amount-received').value);
+    let change = given - owed;
+
+    if (owed < 0 || given < 0) {
+        document.getElementById('error').innerHTML = 'No negative numbers, please!';
+        return;
+    } else if (change < 0 || isNaN(change)) {
+        document.getElementById('error').innerHTML = 'Oops, customer did not give enough money';
+        return;
+    } else {
+        document .getElementById('success').innerHTML = `The customer should receive $${change.toFixed(2)}`;
+    }
+
+    for (let i=0;i<money.length;i++) {
+        let currChange = Math.floor(change.toFixed(2) / money[i][1]);
+        document.getElementById(money[i][0]+"-output").innerHTML = currChange;
+        if (currChange != 0) {
+            document.getElementById(money[i][0]+"-output").animate([
+                {"fontSize":"1em"},
+                {"fontSize" :"5em"},
+                {"fontSize":"1em"}
+            ],500,"swing", () => {return true;});
+        }
+        change -= currChange*money[i][1];
+    }
+
+ }
